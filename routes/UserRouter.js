c73b05367570c53d49d2466efbc6bfe714737a4d
mfpc1500@gmail.com <mfpc1500@gmailcom>
2022-02-04 20:53:13 -0500

    actu

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -5,7 +5,7 @@ var express = require("express");
 var UserController = require('../controllers/UserController');
 
 var router = express.Router();
-
+//rutas para uso de la api
 router.get('/test',UserController.test);
 router.post('/create-admin',UserController.createAdmin);
 router.post('/validate',UserController.login);
